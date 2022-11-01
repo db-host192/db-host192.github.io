487bd0a3c5171ab983d7473fb4c1f6fae08b7d33
Webovski <sokri4.developer@gmail.com>
2022-11-01 19:11:25 +0100

    adde

--- dist/custom.js
+++ dist/custom.js
@@ -1,15 +1,15 @@
 function displayToast(text, event_type) {
     switch (event_type) {
         case 'success':
-            new SnackBar({message: text, status: "success", timeout: 4000});
+            new SnackBar({message: text, status: "success", timeout: 4000, position: 'tl'});
             break;
         case 'error':
-            new SnackBar({message: text, status: "error", timeout: 4000});
+            new SnackBar({message: text, status: "error", timeout: 4000, position: 'tl'});
             break;
         case 'info':
-            new SnackBar({message: text, status: "info", timeout: 4000});
+            new SnackBar({message: text, status: "info", timeout: 4000, position: 'tl'});
             break;
         default:
-            new SnackBar({message: text, status: "error", timeout: 4000});
+            new SnackBar({message: text, status: "error", timeout: 4000, position: 'tl'});
     }
 }
